abee9f085206a1be4b9f35234b72c6e9c33873e3
devjuhis <juho.jarvinen1999@gmail.com>
2025-03-07 14:02:08 +0200

    updated about page

--- web/app/src/pages/about.jsx
+++ web/app/src/pages/about.jsx
@@ -1,17 +1,38 @@
-import React from "react";
-import { Container, Typography, Box, Button, Grid } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { Container, Typography, Box, Button, Grid, Modal } from "@mui/material";
 import headerImage from "../images/macrohub-header1.png";
+import mobileHeader from "../images/mobileHeader.png";
 import Footer from "../components/footer";
 
+import { useTheme } from '@mui/material/styles';
+
 const About = () => {
+    const [backgroundImage, setBackgroundImage] = useState(headerImage);
+    const [open, setOpen] = useState(false);
+    const theme = useTheme();
+
+    useEffect(() => {
+        const handleResize = () => {
+            setBackgroundImage(window.innerWidth < 520 ? mobileHeader : headerImage);
+        };
+
+        handleResize();
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
         <Container sx={{ mt: 10 }}>
             <Grid container spacing={4} alignItems="center">
                 <Grid item xs={12} md={6}>
                     <Box
                         sx={{
-                            height: 500,
-                            backgroundImage: `url(${headerImage})`,
+                            padding: 5,
+                            height: { xs: 300, sm: 500 },
+                            backgroundImage: `url(${backgroundImage})`,
                             backgroundSize: "cover",
                             backgroundPosition: "center",
                             borderRadius: 2,
@@ -43,13 +64,49 @@ const About = () => {
                             color="primary"
                             size="large"
                             sx={{ mt: 2 }}
+                            onClick={handleOpen}
                         >
                             Learn More
                         </Button>
                     </Box>
                 </Grid>
             </Grid>
-            <Footer></Footer>
+
+            <Modal open={open} onClose={handleClose} aria-labelledby="modal-title">
+                <Box
+                    sx={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        width: { xs: 300, sm: 400 },
+                        bgcolor: theme.palette.background.default,
+                        boxShadow: "0px 2px 30px rgba(189, 179, 179, 0.6)",
+                        p: { xs: 4, sm: 10 },
+                        borderRadius: 2,
+                    }}
+                >
+                    <Typography id="modal-title" variant="h6" component="h2">
+                        How does it work?
+                    </Typography>
+                    <Typography sx={{ mt: 2 }}>
+                        1. Download the client to your computer <br />
+                        2. Create account at the website <br />
+                        3. Choose macros you want to use inside the website <br />
+                        4. Log in to the client and start using macros <br />
+                    </Typography>
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        sx={{ mt: 3 }}
+                        onClick={handleClose}
+                    >
+                        Close
+                    </Button>
+                </Box>
+            </Modal>
+
+            <Footer />
         </Container>
     );
 };
