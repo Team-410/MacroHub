0ab2f4fa4b4ac807b44c48e02bc3d30bda59acdb
Kaito Stenroos <kaito.stenroos@outlook.com>
2025-04-01 09:05:17 +0300

    Change eslint rules for the backend

--- web/app/backend/eslint.config.js
+++ web/app/backend/eslint.config.js
@@ -6,5 +6,13 @@ import js from "@eslint/js";
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"] },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    rules: {
+      "no-unused-vars": "off",
+      "no-undef": "off"
+    }
+  },
 ]);
