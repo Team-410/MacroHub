01deff70a9966f6967bcdff6a13092c9b2921f62
devjuhis <juho.jarvinen1999@gmail.com>
2025-04-22 09:58:13 +0300

    updated vite config

--- web/app/vite.config.js
+++ web/app/vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        historyApiFallback: true,
-        proxy: {
-            "/api": "http://localhost:5000",
-        },
+  plugins: [react()],
+  appType: 'spa',
+  server: {
+    proxy: {
+      "/api": "http://localhost:5000",
     },
+  },
 });
