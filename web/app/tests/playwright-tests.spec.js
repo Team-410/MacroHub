1e3bb09e77fc1cd8d29da363b48c0d6ee02b7927
Kaito Stenroos <kaito.stenroos@outlook.com>
2025-05-06 08:50:59 +0300

    Implement some playwright tests

--- web/app/tests/playwright-tests.spec.js
+++ web/app/tests/playwright-tests.spec.js
@@ -34,25 +34,176 @@ test('macropage loads', async ({ page }) => {
     await page.goto(`${BASE_URL}/macro/1`);
     await expect(page.locator('h4')).toBeVisible();
 });
+
 // Authentication testing
-test('require correct credentials before login', async ({ page }) => {
+test('check empty fields before login', async ({ page }) => {
     page.on('dialog', async dialog => {
         expect(dialog.message()).toBe('Fill all fields');
         await dialog.dismiss();
     });
 
     await page.goto(`${BASE_URL}/login`);
-    const usernameInput = page.locator('input[name="email"]');
+    const emailInput = page.locator('input[name="email"]');
     const passwordInput = page.locator('input[name="password"]');
     const loginButton = page.locator('button[type="submit"]');
 
-    // Ensure input fields are empty
-    await usernameInput.fill('');
-    await passwordInput.fill('erere');
+    await emailInput.fill('');
+    await passwordInput.fill('');
 
-    // Trigger the form submission by explicitly clicking the login button
-    // Ensure the button is enabled and visible before clicking
     await expect(loginButton).toBeEnabled();
     await expect(loginButton).toBeVisible();
     await loginButton.click();
+});
+
+test('check empty fields before register', async ({ page }) => {
+    page.on('dialog', async dialog => {
+        expect(dialog.message()).toBe('Fill all fields');
+        await dialog.dismiss();
+    });
+
+    await page.goto(`${BASE_URL}/register`);
+    const emailInput = page.locator('input[name="email"]');
+    const passwordInput = page.locator('input[name="password"]');
+    const usernameInput = page.locator('input[name="fullname"]')
+    const registerButton = page.locator('button[type="submit"]');
+
+    await emailInput.fill('');
+    await passwordInput.fill('');
+    await usernameInput.fill('')
+
+    await expect(registerButton).toBeEnabled();
+    await expect(registerButton).toBeVisible();
+    await registerButton.click();
+});
+
+test('email must contain @ before register', async ({ page }) => {
+    page.on('dialog', async dialog => {
+        expect(dialog.message()).toBe('Email must contain @');
+        await dialog.dismiss();
+    });
+
+    await page.goto(`${BASE_URL}/register`);
+    const emailInput = page.locator('input[name="email"]');
+    const passwordInput = page.locator('input[name="password"]');
+    const usernameInput = page.locator('input[name="fullname"]')
+    const registerButton = page.locator('button[type="submit"]');
+
+    await emailInput.fill('email');
+    await passwordInput.fill('passwd');
+    await usernameInput.fill('user')
+
+    await expect(registerButton).toBeEnabled();
+    await expect(registerButton).toBeVisible();
+    await registerButton.click();
+});
+
+//rest of authentication testing is dependent of credentials set with this test
+test('register account with correct credentials', async ({ page }) => {
+    page.on('dialog', async dialog => {
+        expect(dialog.message()).toBe('User registered succesfully');
+        await dialog.dismiss();
+    });
+
+    await page.goto(`${BASE_URL}/register`);
+    const emailInput = page.locator('input[name="email"]');
+    const passwordInput = page.locator('input[name="password"]');
+    const usernameInput = page.locator('input[name="fullname"]')
+    const registerButton = page.locator('button[type="submit"]');
+
+    //make sure these credentials are not in the database already
+    await emailInput.fill('user@exampleemail.somethings');
+    await passwordInput.fill('passwd123');
+    await usernameInput.fill('makesurethisnameusernameisnotalreadyinthedbs');
+
+    await expect(registerButton).toBeEnabled();
+    await expect(registerButton).toBeVisible();
+    await registerButton.click();
+});
+
+//this test is dependent on creadentials provided provided in test "register account with correct credentials"
+test('login with registered credentials', async ({ page }) => {
+    page.on('dialog', async dialog => {
+        expect(dialog.message()).toBe('Login succesful!');
+        await dialog.dismiss();
+    });
+
+    await page.goto(`${BASE_URL}/login`);
+    const emailInput = page.locator('input[name="email"]');
+    const passwordInput = page.locator('input[name="password"]');
+    const loginButton = page.locator('button[type="submit"]');
+
+    await emailInput.fill('user@exampleemail.something');
+    await passwordInput.fill('passwd123');
+
+    await expect(loginButton).toBeEnabled();
+    await expect(loginButton).toBeVisible();
+    await loginButton.click();
+});
+
+test('prevent login with wrong passwd', async ({ page }) => {
+    page.on('dialog', async dialog => {
+        expect(dialog.message()).toBe('Virhe kirjautumisessa: Check email or password');
+        await dialog.dismiss();
+    });
+
+    await page.goto(`${BASE_URL}/login`);
+    const emailInput = page.locator('input[name="email"]');
+    const passwordInput = page.locator('input[name="password"]');
+    const loginButton = page.locator('button[type="submit"]');
+
+    await emailInput.fill('user@exampleemail.something');
+    await passwordInput.fill('passwd123wrong');
+
+    await expect(loginButton).toBeEnabled();
+    await expect(loginButton).toBeVisible();
+    await loginButton.click();
+});
+
+test('prevent login with wrong email', async ({ page }) => {
+    page.on('dialog', async dialog => {
+        expect(dialog.message()).toBe('Virhe kirjautumisessa: Check email or password');
+        await dialog.dismiss();
+    });
+
+    await page.goto(`${BASE_URL}/login`);
+    const emailInput = page.locator('input[name="email"]');
+    const passwordInput = page.locator('input[name="password"]');
+    const loginButton = page.locator('button[type="submit"]');
+
+    await emailInput.fill('user@exampleemail.something.wrong');
+    await passwordInput.fill('passwd123');
+
+    await expect(loginButton).toBeEnabled();
+    await expect(loginButton).toBeVisible();
+    await loginButton.click();
+});
+
+test('login and logout', async ({ page }) => {
+    page.on('dialog', async dialog => {
+        expect(dialog.type()).toBe("alert");
+        expect(dialog.message()).toBe('Login succesful!');
+        await dialog.accept();
+    });
+
+    await page.goto(`${BASE_URL}/login`);
+    const emailInput = page.locator('input[name="email"]');
+    const passwordInput = page.locator('input[name="password"]');
+    const loginButton = page.locator('button[type="submit"]');
+
+    await emailInput.fill('user@exampleemail.somethings');
+    await passwordInput.fill('passwd123');
+
+    await expect(loginButton).toBeEnabled();
+    await expect(loginButton).toBeVisible();
+    await loginButton.click();
+
+    await page.waitForURL(BASE_URL);
+    await page.waitForLoadState('domcontentloaded');
+    const logoutButton = page.getByRole('button', { name: /logout/i });
+    await expect(logoutButton).toBeVisible();
+    await logoutButton.click();
+    await page.waitForURL(BASE_URL);
+    await page.waitForLoadState('domcontentloaded');
+    const registerButton = page.getByRole('link', { name: /Register/i });
+    await expect(registerButton).toBeVisible();
 });
